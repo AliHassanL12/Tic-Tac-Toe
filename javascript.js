53f1a6eca6289883e8efacfa7198d5492dd6e211
AliHassanL12 <liaqqahl@gmail.com>
2023-12-28 00:52:05 +0000

    Create skeleton HTML, CSS, JS

---
+++ javascript.js
@@ -0,0 +1,22 @@
+
+const gameBoard = (function () {
+    let array = ["-", "-", "-", "-", "-", "-", "-", "-", "-",];
+    return {
+        array
+    };
+})();
+
+function players() {
+    const player1 = "X";
+    const player2 = "O";
+    return {
+        player1,
+        player2
+    };
+}
+
+function gameFlow() {
+    return {
+
+    };
+}
